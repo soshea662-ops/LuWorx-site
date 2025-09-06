63381cdb40176c2ee77c501c8354b2307c0d7391
soshea662-ops <soshea662@gmail.com>
2025-09-05 22:10:56 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 "use client"
 
-import { Card, CardContent, CardHeader, CardTitle } from "./components/ui/card"
 import { Button } from "./components/ui/button"
 import Link from "next/link"
 import { motion } from "framer-motion"
+
 
 function NavHeader() {
   return (
