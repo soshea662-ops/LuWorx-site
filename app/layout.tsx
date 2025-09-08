366f78405a166d608baa58221e50d67368c0fa44
soshea662-ops <soshea662@gmail.com>
2025-09-08 15:54:50 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import "./globals.css";
 import NavHeader from "./components/NavHeader";
+import { ReactNode } from "react";
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className="bg-black text-white">
